4c752eba554e1b23c337b6931cb45ac2b710bf4f
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-03-28 15:29:39 +0900

    Initial Commit

---
+++ Prime_Number02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prime_Number02
+//
+//  Created by D7702_09 on 2018. 3. 28..
+//  Copyright © 2018년 lse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
